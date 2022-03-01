31d31a2102b6ca5ae585766af379dca8c61b0c88
Ritik Patel <ritikptl05@gmail.com>
2022-03-01 23:08:01 +0530

    fix: file path for tests in sonar and jest config

--- jest.config.js
+++ jest.config.js
@@ -117,9 +117,7 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ["<rootDir>/server/tests"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -137,7 +135,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
